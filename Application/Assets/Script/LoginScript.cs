da0520f59b7221d060cc60027f497a0008b6e5eb
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2018-11-25 13:36:26 +0800

    Login Save user

---
+++ Application/Assets/Script/LoginScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LoginScript : MonoBehaviour {
+    public Text UserField;
+	// Use this for initialization
+    string user = "Character";
+	void Start () {
+        if (PlayerPrefs.HasKey(user))
+      {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      }
+	}
+    public void Character1()    
+    {
+        PlayerPrefs.SetString(user, "CHRSTNV");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        UserField.text = PlayerPrefs.GetString(user).ToString();
+      //  Debug.Log(PlayerPrefs.GetString(user).ToString());  
+    }
+	
+    public void Character2()    
+    {
+        PlayerPrefs.SetString(user, "CHRSTNV2");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        UserField.text = PlayerPrefs.GetString(user).ToString();
+      //  Debug.Log(PlayerPrefs.GetString(user).ToString());  
+    }
+
+    public void Character3()    
+    {
+        PlayerPrefs.SetString(user, "CHRSTNV3");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        UserField.text = PlayerPrefs.GetString(user).ToString();
+      //  Debug.Log(PlayerPrefs.GetString(user).ToString());  
+    }
+
+    public void Character4()    
+    {
+        PlayerPrefs.SetString(user, "CHRSTNV4");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        UserField.text = PlayerPrefs.GetString(user).ToString();
+      //  Debug.Log(PlayerPrefs.GetString(user).ToString());  
+    }
+
+}
--- Application/Assets/Script/MenuSCR.cs
+++ Application/Assets/Script/MenuSCR.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class MenuSCR : MonoBehaviour {
+    public Text Username;
+    string user = "Character";
+    void Start() {
 
-
+      Debug.Log(PlayerPrefs.GetString(user).ToString());
+      Username.text = PlayerPrefs.GetString(user).ToString();
+    }
     public void PlayBTN()
     {
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void StatsBTN()
